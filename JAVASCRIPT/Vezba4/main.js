3a1561441a82338a62937d705e5c9d62e51ddff6
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2023-11-19 23:08:57 +0100

    Update

---
+++ JAVASCRIPT/Vezba4/main.js
@@ -0,0 +1,11 @@
+const nizaGradovi = [{grad:"Atina", populacija:1100000},{grad:"Ankara", populacija:1455000},{grad:"Berlin", populacija:2600000},{grad:"Abdal", populacija:650000},{grad:"Solun", populacija:850000},{grad:"London", populacija:10500000},{grad:"Moskva", populacija:12500000}]
+
+const selectGradovi = nizaGradovi.filter(function (nizaGradovi) {
+    if (nizaGradovi.populacija >= 999999 && nizaGradovi.grad [0] ==="A") {
+      return true;
+    }
+  });  
+       console.log(selectGradovi);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
