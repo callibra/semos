44997b8b0d1b3f33fc630fd8b06ffaea417c8b4d
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-04-26 21:02:07 +0200

    Update

---
+++ MICROSERVICES/task-1/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./pkg/db/index');
+const courseRoutes = require('./routes/courseRoutes');
+const academyRoutes = require('./routes/academyRoutes');
+
+const app = express();
+
+db.init();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
+
+app.use('/courses', courseRoutes);
+app.use('/academies', academyRoutes);
+
+app.listen(process.env.PORTAUTH, (err) => {
+    if (err) {
+      console.log('Could not start service');
+    }
+    console.log(`service started successfully on port ${process.env.PORTAUTH}`);
+  });
+
